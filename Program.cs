ae3747a4556e635447662aca532cb6faa87f2563
imaraabdullah0 <abdulluhemara5200@yahoo.com>
2022-06-11 02:13:50 +0200

    push

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace abdullah.imara
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
